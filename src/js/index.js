85aaf0805905d066fbf637ea73470fe43accb396
orchidenver <setyourgoals89@gmail.com>
2021-07-20 16:15:39 +0300

    slider added

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+import $ from 'jquery';
+import 'slick-carousel';
+
+$('.slider__slides').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow: '.button-left',
+    nextArrow: '.button-right',
+
+});
